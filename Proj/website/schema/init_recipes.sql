dda37cf369027673869152c1ef6c3cb5bcbbc5f3
nielsVonHolck <niels_holck@yahoo.com>
2023-06-11 17:11:59 +0200

    sql files + html for matches and closest

---
+++ Proj/website/schema/init_recipes.sql
@@ -0,0 +1,6 @@
+INSERT INTO recipes (recipename, ingr, rcplink) 
+VALUES 
+    ('Bruschetta Pizza', ARRAY['pizza dough', 'tomatoes', 'garlic', 'basil', 'olive oil', 'mozarella'], 'https://www.allrecipes.com/bruschetta-pizza-recipe-7503843'),
+    ('No-Cook Tomato Basil Pasta', ARRAY['Tomato', 'garlic', 'parmesan', 'butter', 'olive oil', 'lemon', 'balsamic vinegar', 'honey', 'spaghetti', 'basil'], 'https://www.simplyrecipes.com/no-cook-tomato-sauce-recipe-5509350'),
+    ('Buddy\’s potato salad', ARRAY['potatoes', 'herbs', 'youghurt', 'lemon', 'olive oil'], 'https://www.jamieoliver.com/recipes/potato-recipes/buddy-s-potato-salad/'),
+    ('Deep fried sausage', ARRAY['sausages', 'oil'], 'https://cookpad.com/pk/recipes/3294239-deep-fried-sausage');
---
+++ Proj/website/schema/init_users.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (usrname, pass, fridgelist) 
+VALUES ('David', 
+        '1234', 
+        ARRAY['egg', 'cream', 'pie dough', 'salt']
+        );
+
+INSERT INTO users (usrname, pass, fridgelist) 
+VALUES ('Niels', 
+        '1234', 
+        ARRAY['flour', 'carrots', 'salt']
+        );
